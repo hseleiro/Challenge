b8d5c93469ab9b720439dda9ea3167bde5ccaadd
Hugo Seleiro <hseleiro@gmail.com>
2021-01-18 09:42:18 +0000

    Evolution - add vehicles

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,119 @@
 <template>
   <section id="app">
-    
+    <Categories v-on:filter-option="filterOptions"/>
+    <Gallery v-bind:filteredVehicles=filteredVehicles />
   </section>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+  import Vue from 'vue';
+  import Categories from "@/components/Categories.vue";
+  import Gallery from "@/components/Gallery.vue";
+  import { uuid } from 'vue-uuid';
+  import vehiclesJson from "./static/vehicles.json"
 
-
-export default Vue.extend({
-  name: 'App',
-  components: {
+  type VehiclesArrayInterface = {
+    id: number,
+    driverName: string
+    vehicleFeatures: [
+      {
+        licencePlate: string,
+        manufacturer: string,
+        acquisitionDate: string,
+        odometer: number,
+        hasInsurance: boolean,
+        dateNextInspection: string,
+      }
+    ],
+    time: Date
   }
-});
+
+  type vehicleFeatures = {
+    licencePlate: string,
+    manufacturer: string,
+    acquisitionDate: string,
+    odometer: number,
+    hasInsurance: boolean,
+    dateNextInspection: string,
+  }
+
+  export default Vue.extend({
+    name: 'App',
+    components: {
+      Categories,
+      Gallery
+    },
+    data() {
+      return {
+        filteredVehicles: Object as () => Array<VehiclesArrayInterface>,
+        vehicles: vehiclesJson
+      }
+    },
+    beforeMount(): void {
+      // Check if vehicles array is store in sessionStore.
+      if (sessionStorage.getItem('vehicles') === null) {
+        this.setSessionStorage();
+      }
+      this.setTimeInterval();
+    },
+    methods: {
+      // Set session storage
+      setSessionStorage(): void {
+        // Set time when vehicles array is storage, with 30 minutes plus.
+        const time = new Date();
+        time.setMinutes((time.getMinutes() + 30));
+
+        // Push new proprety to vehicles array.
+        this.vehicles.push(time);
+
+        // Parse and save vehicles array on sessionStorage.
+        const vehiclesParsed = JSON.stringify(this.vehicles);
+        sessionStorage.setItem('vehicles', vehiclesParsed);
+      },
+      // Set a time interval to watch if the sessionStorage as expired.
+      setTimeInterval(): void {
+        const watchInterval = 15*60*1000;
+        setInterval(() =>{ this.watchIfSessionStorageAsExpired(); }, watchInterval );
+      },
+      // Watch if session as expired.
+      watchIfSessionStorageAsExpired(): void {
+        const vehiclesParsed = JSON.parse(sessionStorage.getItem('vehicles'));
+        // If moment time is greater 30 minutes plus than where vehicles array where store in sessionStorage
+        // we remove the array from local storage.
+        // console.log('avé', vehiclesParsed[50] < new Date())
+        if (vehiclesParsed[50] < new Date()) {
+          localStorage.removeItem("vehicles");
+          this.setSessionStorage();
+        }
+      },
+      // Filter category options.
+      filterOptions(checkedValues: number[]): void {
+        if(checkedValues && this.vehicles.length) {
+          this.filteredVehicles = this.vehicles.filter((vehicle: { vehicleFeatures: vehicleFeatures[]; }) => {
+            if(vehicle.vehicleFeatures) {
+              return vehicle.vehicleFeatures
+              .map(vehicleFeatures => vehicleFeatures.odometer)
+              .find(featureOdometer => {
+                return checkedValues.find((odometerOption: number) => {
+                  return featureOdometer >= odometerOption;
+                });
+              });
+            }
+          });
+        }
+      }
+    }
+  });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
+
---
+++ src/components/Categories.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <div class="odometer-options-header-title">
+            <h2>Odometer Options</h2>
+        </div>
+        <div class="odometer-options-container">
+            <div class="option" v-for="option in odometerOptions">
+                <label>
+                    <input type="checkbox"
+                           name="options"
+                           v-model="option.checked"
+                           @change="onFilterOdometer"/>
+                    <span class="option-name">{{option.name}}</span>
+                </label>
+            </div>
+        </div>
+        <div v-if="!showVehicles" class="odometer-options-container-title">
+            <h2>Please select an option</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Categories",
+        data() {
+            return {
+                showVehicles : false,
+                odometerOptions: [
+                    { name: "> 75.000Km", value: 75.000, checked: false },
+                    { name: ">= 130.000Km", value: 130.000, checked: false },
+                    { name: ">= 250.000Km", value: 250.000, checked: false },
+                ]
+            }
+        },
+        methods: {
+            onFilterOdometer() {
+                this.$emit('filter-option',this.odometerOptions.filter((opt) => opt.checked).map(opt => opt.value));
+                this.showVehicles = !this.showVehicles;
+            },
+
+        }
+    }
+</script>
+
+<style scoped>
+    .odometer-options-header-title {
+        background: #1abc9c;
+        padding: 50px;
+    }
+
+    .odometer-options-header-title h2 {
+        color: #ffffff;
+        font-size: 2em;
+    }
+
+    .odometer-options-container-title {
+        padding: 40px 0 0 0;
+    }
+
+    .odometer-options-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        padding: 50px 0;
+    }
+
+    .option {
+        margin: 0 10px;
+        padding: 10px;
+        border-radius: 10px;
+        background-color: #1abc9c;
+    }
+
+    .option-name {
+        font-size: 1.1em;
+        margin-left: 5px;
+        color: #ffffff;
+        font-weight: bold;
+    }
+
+</style>
